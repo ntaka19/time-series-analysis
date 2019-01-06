566454ccef1944bf4712e0c35bc35688250a3c39
Naruaki Takano <naruakitakano@gmail.com>
2019-01-06 23:48:58 +0900

    correlation dimension

---
+++ nonlinear-data-analysis/correlation-dim.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def cordim(N,m,f):
+    count=0
+    Clist=[]
+#M=len(xx)
+    #xx=np.array([np.array([ f[m*i+j] for j in range(m)] ) for i in range(int(N/m)) ])
+
+    tau=10
+    xx=np.array([np.array([ f[i+tau*j] for j in range(m)] ) for i in range(N-m*tau) ])
+
+    M=10000
+    rlist=[0.1,0.5, 1.0, 5, 10, 100,1000]
+
+    for r in rlist:
+        count=0
+        for i in range(M):
+            if(i%100==0):
+                print(i)
+            x1=xx[i]
+            for j in range(M):
+                x2=xx[j]
+                if(  np.sum(abs(x1-x2)) <r):
+                    count+=1
+
+        Clist.append(count/pow(N,2))
+
+    return rlist,Clist
+
+
+
+def savefig(m,rlist,Clist):
+    plt.figure()
+    plt.plot(np.log10(rlist), np.log10(Clist), 'bo')
+    plt.plot(np.log10(rlist), np.log10(rlist)*0.9-4 , label="0.9x-4.0")
+    plt.xlabel("log(r)")
+    plt.ylabel("log C(r)")
+    plt.legend()
+    plt.title("Correlation Dimension")
+    plt.savefig( 'cordim-dim{dim}.png'.format(dim=m))
+    plt.close()
+
+
+
+def calculate():
+    df = pd.read_csv(r'northwind20041025130835.csv',header=None)
+    t=np.arange(0,len(df.values))
+    f=np.array(df.values,dtype='float').flatten()
+    N=len(df)
+    for m in [3]:
+        rlist,Clist=cordim(N,m,f)
+        savefig(m,rlist,Clist)
+        
+    
+if __name__ == '__main__':
+    calculate()
+
+
